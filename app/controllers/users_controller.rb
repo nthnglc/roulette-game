2aefde5b49ccdffce0a9750e1c27ed681601850c
Javier Luco <javierluco.dd2@gmail.com>
2021-10-27 20:10:06 -0300

    add init balance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,11 +22,12 @@ class UsersController < ApplicationController
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
+    @user.balance += 10000
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
+        format.html { redirect_to @user, notice: "Usuario creado" }
+        format.json { render :edit, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -38,8 +39,8 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
+        format.html { redirect_to @user, notice: "Usuario actualizado" }
+        format.json { render :edit, status: :ok, location: @user }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -51,7 +52,7 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+      format.html { redirect_to users_url, notice: "Usuario Eliminado" }
       format.json { head :no_content }
     end
   end
